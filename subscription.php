92c97eb8539c91dc2bc8f6478d78062f443fbe6e
Srdjan Maravic <srdjanmaravic@gmail.com>
2021-03-09 22:04:34 +0100

    update

--- subscription.php
+++ subscription.php
@@ -1,3 +1,3 @@
 <?php include"partials/header.php"; ?>
-    <h1 style="font-size: 9rem; text-aligne:center;">Subscriptions</h1>
+    <h1 style="font-size: 9rem; text-aligne:center;">Subscription</h1>
 <?php include"partials/footer.php"; ?>
