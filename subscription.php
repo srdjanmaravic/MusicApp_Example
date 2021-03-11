eecb99c957306be22a70e0fbe2b9e35f63e1a7a8
Srdjan Maravic <srdjanmaravic@gmail.com>
2021-03-11 20:50:15 +0100

    finished

--- subscription.php
+++ subscription.php
@@ -1,5 +1,26 @@
 <?php include"partials/header.php"; ?>
-   
+   <!-- Header -->
+   <section class="subscription">
+        <article class="container">
+            <div>
+            <h2>Subscription from $15/month</h2>
+                <h4>Start a free trial now</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda architecto nisi incidunt, laborum, alias eum eos labore, cumque iusto minima cupiditate tenetur? Commodi, repellat! Necessitatibus libero laborum tempora nihil accusantium?</p>
+                <button>Start free trial</button>
+            </div>
+            <div>
+                <ul>
+                    <li>Play any track</li>
+                    <li>Listen offline</li>
+                    <li>No add interruptions</li>
+                    <li>Unlimited skips</li>
+                    <li>High quality</li>
+                    <li>Shuffle play</li>
+                </ul>
+            </div>
+        </article>
+   </section>
+   <!-- Concept -->
     <section class="concept container">
         <article>
             <h2>Our Concept & artists</h2>
